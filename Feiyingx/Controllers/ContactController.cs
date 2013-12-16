7d7ccf88da42e0723bf8bac3700fa2406d5c3adb
Feiyingx <feiyingx@gmail.com>
2013-12-16 00:43:39 -0800

    email contact form terminal ui

---
+++ Feiyingx/Controllers/ContactController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Feiyingx.Controllers
+{
+    public class ContactController : Controller
+    {
+        public ActionResult Email()
+        {
+            return View();
+        }
+
+        public ActionResult Text()
+        {
+            return View();
+        }
+
+        public ActionResult Video()
+        {
+            return View();
+        }
+    }
+}
